9a9663f471dac51c52f5fce670a8db6b038a32ae
Yerkebulan <erke.zhumagali@gmail.com>
2018-03-15 23:19:18 +0600

    Updated

--- Matlab Examples/3.m
+++
@@ -1,4 +0,0 @@
-f=@(x,y) x^2-3*x*y+y^2-3*y
-[x,y]=Eulerh(f,0,2,0.05,3)
-plot(x,y)
-title('Baibaktiyeva Aksunkar 3.1 #3 : dy+3y=x^2-3xy+y^2')
--- Matlab Examples/clw1.m
+++
@@ -1,6 +0,0 @@
-[x,y]=meshgrid(-3:.3:3, -2:.2:2);
-dy=x+sin(y)
-dx=ones(size(dy))
-dy1=dy./(sqrt(dx.^2+dy.^2))
-dx1=dx./(sqrt(dx.^2+dy.^2))
-quiver(x,y,dx1,dy1)
